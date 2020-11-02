8e7a06a6f232b3f9d3c56dba42d2afc978b3ceab
Ahmet Alp Balkan <ahmetb@google.com>
2020-11-01 19:39:43 -0800

    add terraform specs
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ tf/main.tf
@@ -0,0 +1,63 @@
+provider google {
+  project = "ahmet-personal-api"
+}
+
+resource "google_service_account" "default" {
+  account_id = "baklava"
+}
+
+data "google_cloud_run_service" "default" {
+  name     = "baklava"
+  location = "us-central1"
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name        = "baklava"
+  description = "updates baklava prices"
+  schedule    = "15 2 * * *"
+  time_zone   = "America/Los_Angeles"
+  region      = "us-east1" # because that's where GAE zone is
+
+  attempt_deadline = "120s"
+  retry_config {
+    retry_count = 0
+  }
+  http_target {
+    http_method = "GET"
+    # TODO pull the url from the "data"
+    uri = "https://${element(data.google_cloud_run_service.default.status, 0).url}/run"
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+    }
+  }
+}
+
+data "google_iam_policy" "invoker" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "serviceAccount:${google_service_account.default.email}",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "policy" {
+  location    = data.google_cloud_run_service.default.location
+  project     = data.google_cloud_run_service.default.project
+  service     = data.google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.invoker.policy_data
+}
+
+output "scheduler_job" {
+  value = google_cloud_scheduler_job.job.id
+}
+
+output "service_account_email" {
+  value = google_service_account.default.email
+}
+
+
+output "cloud_run_url" {
+  value = element(data.google_cloud_run_service.default.status, 0).url
+}
