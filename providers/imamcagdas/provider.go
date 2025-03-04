5aa549d98ae310bb0a6364c5b7b0717fecebc980
Eminks <1908412+eminks@users.noreply.github.com>
2025-03-04 23:29:33 +0000

    ImamCagdasProvider update

--- providers/imamcagdas/provider.go
+++ providers/imamcagdas/provider.go
@@ -19,13 +19,13 @@ func (i ImamCagdasProvider) Name() string {
 }
 
 func (i ImamCagdasProvider) FistikliBaklava() (*money.Money, error) {
-	return genericparser.GenericParser{}.FromURL(`.product-current-price`, fistikliBaklavaURL)
+	return genericparser.GenericParser{}.FromURL(`span[data-toggle="price-sell-vat"]`, fistikliBaklavaURL)
 }
 
 func (i ImamCagdasProvider) FistikDolama() (*money.Money, error) {
-	return genericparser.GenericParser{}.FromURL(`.product-current-price`, fistikDolamaURL)
+	return genericparser.GenericParser{}.FromURL(`span[data-toggle="price-sell-vat"]`, fistikDolamaURL)
 }
 
 func (i ImamCagdasProvider) KuruBaklava() (*money.Money, error) {
-	return genericparser.GenericParser{}.FromURL(`.product-current-price`, kuruBaklavaURL)
+	return genericparser.GenericParser{}.FromURL(`span[data-toggle="price-sell-vat"]`, kuruBaklavaURL)
 }
